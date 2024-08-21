4b069d65031b1715aec95823212300ff3b3fb762
saad6742988 <ms6742988@gmail.com>
2024-08-21 21:37:20 +0200

    Binding to Attributes

---
+++ 5. Bindning to Attributes/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+  <!-- Binding headings tag id using Bind directive -->
+   <h2 v-bind:id="headingId">This is my Heading</h2>
+
+   <!--Binding button poperty disable-->
+   <button v-bind:disabled="disable">My Disable Button</button>
+   <!--Spacing between buttons-->
+   <br>
+   <br>
+   <!--Using ! will make disable false-->
+   <button v-bind:disabled="!disable">My Enable Button</button>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      greet:"Hello",
+      name:"<b>Saad</b>",
+      threat:`<a href="#" onclick = "alert('You have been hacked!')">Win a Prizes!</a>`,
+      headingId:"MyHead",
+      disable:true
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
